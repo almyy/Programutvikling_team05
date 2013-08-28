de1712de13d0a6fdd74f1d09e38a133d9d76cf6a
Rino <Rino@158.38.185.226>
2013-08-28 15:23:17 +0200

    Textures for visionShooter

--- PVUgame/src/no/hist/gruppe5/pvu/Assets.java
+++ PVUgame/src/no/hist/gruppe5/pvu/Assets.java
@@ -2,6 +2,7 @@ package no.hist.gruppe5.pvu;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 /**
@@ -13,14 +14,23 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public class Assets {
 
     public static TextureRegion testRegion;
+    public static TextureRegion visionShooterRegion; 
+    public static TextureRegion visionShooterDocumentRegion; 
+    public static TextureRegion visionShooterShipRegion; 
 
     public static void load() {
         Texture testTexture = new Texture(Gdx.files.internal("data/square.png"));
         testTexture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
-
+        
         testRegion = new TextureRegion(testTexture, 0, 0, 100, 100);
+        
+        
+        TextureAtlas visionShooterAtlas = new TextureAtlas(Gdx.files.internal("data/VisionShooterTexture.pack"));
+        
+        visionShooterRegion = visionShooterAtlas.findRegion("VisionShooterTexture"); 
+        visionShooterDocumentRegion = visionShooterAtlas.findRegion("Document");
+        visionShooterShipRegion = visionShooterAtlas.findRegion("VisionShooterShip");
     }
-
     public static void dispose() {
     }
 }
