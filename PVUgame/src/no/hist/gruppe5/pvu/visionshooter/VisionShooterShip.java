d64bb10aaaf8ab6a41c023c0261c20e478f75380
Rino <Rino@158.38.185.226>
2013-08-29 10:54:04 +0200

    Textures for visionShooter

--- PVUgame/src/no/hist/gruppe5/pvu/visionshooter/VisionShooterShip.java
+++ PVUgame/src/no/hist/gruppe5/pvu/visionshooter/VisionShooterShip.java
@@ -4,25 +4,54 @@
  */
 package no.hist.gruppe5.pvu.visionshooter;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.*;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import no.hist.gruppe5.pvu.Assets;
 
 public class VisionShooterShip{
     
     private Sprite shipSprite;
-    private float shipHeight = 0.3f;
-    private float shipWidth = 0.3f;
+    
+    private float shipHeight = 0.5f;
+    private float shipWidth = 0.5f;
     
     private float shipX = 0.149f;
+    private float shipY = 1; 
+    
+    private float shipSpeed = 2.2f;  
     
     public VisionShooterShip(){
         shipSprite = new Sprite(Assets.visionShooterShipRegion); 
         shipSprite.setSize(shipHeight, shipWidth);
-        shipSprite.setPosition(shipX,1);
+        shipSprite.setPosition(shipX,shipY);
     }
 
     public Sprite getShipSprite() {
         return shipSprite;
+    }
+
+    void draw(SpriteBatch batch) {
+        shipSprite.draw(batch);
+    }
+
+    void update(float delta) {
+        if(Gdx.input.isKeyPressed(Keys.DPAD_UP)){
+            shipY += shipSpeed*delta;
+        }
+        if(Gdx.input.isKeyPressed(Keys.DPAD_DOWN)){
+            shipY -= shipSpeed*delta; 
+        }
+        shipSprite.setPosition(shipX, shipY);
+    }
+
+    public float getShipX() {
+        return shipX;
+    }
+
+    public float getShipY() {
+        return shipY;
     }
     
 }
