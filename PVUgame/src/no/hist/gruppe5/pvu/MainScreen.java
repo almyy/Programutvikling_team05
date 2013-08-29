d53bd9498409bc3087a1a7536bfd8beafae9742c
Karl J. Overå <karloveraa@gmail.com>
2013-08-29 11:53:44 +0200

    Started on physics on main screen

--- PVUgame/src/no/hist/gruppe5/pvu/MainScreen.java
+++ PVUgame/src/no/hist/gruppe5/pvu/MainScreen.java
@@ -1,9 +1,14 @@
 package no.hist.gruppe5.pvu;
 
+import aurelienribon.bodyeditor.BodyEditorLoader;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.utils.TimeUtils;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,6 +18,11 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
  */
 public class MainScreen extends GameScreen {
 
+    private static final float WORLD_TO_BOX = 0.01f;
+    private static final float BOX_TO_WORLD = 100f;
+
+    private World mWorld;
+    private Box2DDebugRenderer mDebugRenderer;
 
     private boolean left = true;
     private boolean up = true;
@@ -23,6 +33,10 @@ public class MainScreen extends GameScreen {
         super(game);
 
 
+        mWorld = new World(new Vector2(0, -1f), true);
+        mDebugRenderer = new Box2DDebugRenderer();
+
+        createTestBody();
 
         // TODO temp
         moveToAssets();
@@ -30,6 +44,7 @@ public class MainScreen extends GameScreen {
 
         mBackground.setSize(PVU.GAME_WIDTH, PVU.GAME_HEIGHT);
         mBackground.setPosition(0, 0);
+
     }
 
     public void moveToAssets() {
@@ -45,11 +60,41 @@ public class MainScreen extends GameScreen {
         batch.begin();
         mBackground.draw(batch);
         batch.end();
+
+        mDebugRenderer.render(mWorld, camera.combined);
+
+
+    }
+
+    private void createTestBody() {
+        BodyEditorLoader loader = new BodyEditorLoader(Gdx.files.internal("data/pvugame.json"));
+        //Ground body
+        BodyDef groundBodyDef =new BodyDef();
+        groundBodyDef.position.set(new Vector2(0f ,0f));
+        Body groundBody = mWorld.createBody(groundBodyDef);
+        PolygonShape groundBox = new PolygonShape();
+        groundBox.setAsBox((camera.viewportWidth), 0.2f);
+        groundBody.createFixture(groundBox, 0.0f);
+
+        //Dynamic Body  
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2);
+        Body body = mWorld.createBody(bodyDef);
+        CircleShape dynamicCircle = new CircleShape();
+        dynamicCircle.setRadius(0.2f);
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = dynamicCircle;
+        fixtureDef.density = 1.0f;
+        fixtureDef.friction = 0.0f;
+        fixtureDef.restitution = 1;
+        body.createFixture(fixtureDef);
+        mDebugRenderer = new Box2DDebugRenderer();
     }
 
     @Override
     protected void update(float delta) {
-
+        mWorld.step(1 / 60f, 6, 2);
     }
 
     @Override
