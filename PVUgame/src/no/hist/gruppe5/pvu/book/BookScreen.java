549b1064ce801ff26ed0997f92a050875647052f
Karl J. Overå <karloveraa@gmail.com>
2013-09-02 16:00:55 +0200

    Added ability to exit book

--- PVUgame/src/no/hist/gruppe5/pvu/book/BookScreen.java
+++ PVUgame/src/no/hist/gruppe5/pvu/book/BookScreen.java
@@ -5,6 +5,7 @@
 package no.hist.gruppe5.pvu.book;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.Texture;
@@ -61,7 +62,9 @@ public class BookScreen extends GameScreen {
 
     @Override
     protected void update(float delta) {
-        Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
+            game.setScreen(PVU.MAIN_SCREEN);
+        }
     }
 
     @Override
