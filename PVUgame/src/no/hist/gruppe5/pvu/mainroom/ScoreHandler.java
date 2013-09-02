fe82156033a2d15460d226355f366f909d4a0a10
l1nnk <linnkri91@gmail.com>
2013-09-02 13:23:09 +0200

    Created a score handler

---
+++ PVUgame/src/no/hist/gruppe5/pvu/mainroom/ScoreHandler.java
@@ -0,0 +1,73 @@
+package no.hist.gruppe5.pvu.mainroom;
+
+/**
+ *
+ * @author linnk
+ */
+public class ScoreHandler {
+
+    private static int[] totalScore;
+    private static boolean completedAllLevels;
+    private static int total;
+
+    public ScoreHandler() {
+        totalScore = new int[5];
+        completedAllLevels = false;
+        total = 0;
+    }
+
+    /**
+     * Checks if all minigames are completed.
+     *
+     * @return true/false
+     */
+    public static boolean checkScore() {
+        for (int i = 0; i < totalScore.length; i++) {
+            if (totalScore[i] == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Updates totalscore based on the result of current minigame.
+     *
+     * @param minigame
+     * @param score
+     * @return if the total score was modified
+     */
+    public static boolean updateScore(int minigame, int score) {
+        if (minigame < totalScore.length) {
+            totalScore[minigame] = score;
+            total+=score;
+            if (checkScore()) {
+                completedAllLevels = true;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     *
+     * @return a grade based on the total score.
+     */
+    public static Character getGrade() {
+        if(total>90){
+            return 'A';
+        }else if(total>80){
+            return 'B';
+        }else if(total>70){
+            return 'C';
+        }else if(total>60){
+            return 'D';
+        }else{
+            return 'E';
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ScoreHandler.getGrade());
+    }
+}
