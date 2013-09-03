1d7ddaacaa080317d71c9f5614d1a4763d716681
l1nnk <linnkri91@gmail.com>
2013-09-03 15:49:45 +0200

    Created soundbutton

--- PVUgame/src/no/hist/gruppe5/pvu/GameScreen.java
+++ PVUgame/src/no/hist/gruppe5/pvu/GameScreen.java
@@ -5,18 +5,27 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.TimeUtils;
 
 /**
- * Created with IntelliJ IDEA.
- * User: karl
- * Date: 8/28/13
- * Time: 9:48 AM
+ * Created with IntelliJ IDEA. User: karl Date: 8/28/13 Time: 9:48 AM
  */
 public abstract class GameScreen implements Screen {
 
     protected PVU game;
     protected SpriteBatch batch;
     protected OrthographicCamera camera;
+    private Button soundButton;
+    private Button noSoundButton;
+    private Stage stage;
+    private long timeSinceLastAction;
 
     public GameScreen(PVU game) {
         this.game = game;
@@ -24,10 +33,16 @@ public abstract class GameScreen implements Screen {
         camera = new OrthographicCamera();
         camera.setToOrtho(false, PVU.GAME_WIDTH, PVU.GAME_HEIGHT);
         batch = new SpriteBatch();
+
+        timeSinceLastAction = 0;
+        stage = new Stage(PVU.GAME_WIDTH * 2.7f, PVU.GAME_HEIGHT * 2.7f, true);
+        initSoundButton();
     }
 
     protected abstract void draw(float delta);
+
     protected abstract void update(float delta);
+
     protected abstract void cleanUp();
 
     protected void clearCamera(float r, float g, float b, float a) {
@@ -44,9 +59,22 @@ public abstract class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        float deltaUpdate =  (delta > 0.1f) ? 0.1f : delta;
+        float deltaUpdate = (delta > 0.1f) ? 0.1f : delta;
         update(deltaUpdate);
         draw(delta);
+
+        if (TimeUtils.millis() - timeSinceLastAction > 400l) {
+            if (soundButton.isPressed()) {
+                soundButton.remove();
+                stage.addActor(noSoundButton);
+                timeSinceLastAction = TimeUtils.millis();
+            }else if(noSoundButton.isPressed()){
+                noSoundButton.remove();
+                stage.addActor(soundButton);
+                timeSinceLastAction = TimeUtils.millis();
+            }
+        }
+        stage.draw();
     }
 
     @Override
@@ -69,5 +97,16 @@ public abstract class GameScreen implements Screen {
 
     @Override
     public void resume() {
+    }
+
+    public void initSoundButton() {
+        TextureAtlas atlas = new TextureAtlas("data/menuButtons/menubuttons.pack");
+        Skin skin = new Skin(atlas);
+        noSoundButton = new Button(skin.getDrawable("nosound.up"));
+        soundButton = new Button(skin.getDrawable("sound.up"));
+        soundButton.setPosition(PVU.GAME_WIDTH * 2.7f - 25, PVU.GAME_HEIGHT * 2.7f - 25);
+        noSoundButton.setPosition(PVU.GAME_WIDTH * 2.7f - 25, PVU.GAME_HEIGHT * 2.7f - 25);
+        stage.addActor(soundButton);
+        Gdx.input.setInputProcessor(stage);
     }
 }
