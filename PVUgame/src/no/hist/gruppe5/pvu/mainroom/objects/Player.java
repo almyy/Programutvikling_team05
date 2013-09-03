1984a3bd7d52c83ce22edc4ab972b2ff2faab3d4
Karl J. Overå <karloveraa@gmail.com>
2013-09-03 11:24:52 +0200

    Added some interactivity to the main room.

--- PVUgame/src/no/hist/gruppe5/pvu/mainroom/objects/Player.java
+++ PVUgame/src/no/hist/gruppe5/pvu/mainroom/objects/Player.java
@@ -1,4 +1,4 @@
-package no.hist.gruppe5.pvu.mainroom;
+package no.hist.gruppe5.pvu.mainroom.objects;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
@@ -9,6 +9,7 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import no.hist.gruppe5.pvu.Assets;
+import no.hist.gruppe5.pvu.mainroom.MainScreen;
 
 /**
  * Created with IntelliJ IDEA.
@@ -20,23 +21,29 @@ import no.hist.gruppe5.pvu.Assets;
 
 public class Player {
 
-    private static final float PLAYER_SIZE = 12f;
+    private final float PLAYER_SIZE;
 
-    private final TextureRegion PLAYER_BACK;
-    private final TextureRegion PLAYER_LEFT;
-    private final TextureRegion PLAYER_RIGHT;
-    private final TextureRegion PLAYER_FRONT;
+    private final Animation PLAYER_BACK;
+    private final Animation PLAYER_LEFT;
+    private final Animation PLAYER_RIGHT;
+    private final Animation PLAYER_FRONT;
+    private final Animation PLAYER_SITTING;
+
+    private boolean mSitting = false;
 
     private SpriteBatch mBatch;
-
     private Body mPlayerBody;
     private Sprite mPlayerSprite;
 
     public Player(World world) {
-        PLAYER_BACK = Assets.getAvatarRegion(Assets.MAIN_AVATAR_BACK);
-        PLAYER_LEFT = Assets.getAvatarRegion(Assets.MAIN_AVATAR_SIDE_LEFT);
-        PLAYER_RIGHT = Assets.getAvatarRegion(Assets.MAIN_AVATAR_SIDE_RIGHT);
-        PLAYER_FRONT = Assets.getAvatarRegion(Assets.MAIN_AVATAR_FRONT);
+        PLAYER_BACK = new Animation(Assets.MAIN_AVATAR_BACK, 2);
+        PLAYER_LEFT = new Animation(Assets.MAIN_AVATAR_SIDE_LEFT, 2);
+        PLAYER_RIGHT = new Animation(Assets.MAIN_AVATAR_SIDE_RIGHT, 2);
+        PLAYER_FRONT = new Animation(Assets.MAIN_AVATAR_FRONT, 2);
+        PLAYER_SITTING = new Animation(Assets.MAIN_AVATAR_SITTING, 2);
+
+        mPlayerSprite = new Sprite(PLAYER_FRONT.getFrame());
+        PLAYER_SIZE = mPlayerSprite.getWidth();
 
         //Dynamic Body
         BodyDef bodyDef = new BodyDef();
@@ -55,9 +62,7 @@ public class Player {
         mPlayerBody.setFixedRotation(true);
         mPlayerBody.setUserData(MainScreen.OBJECT_PLAYER);
 
-        mPlayerSprite = new Sprite(PLAYER_FRONT);
-        mPlayerSprite.setSize(PLAYER_SIZE, ( mPlayerSprite.getHeight() / mPlayerSprite.getWidth() ) * PLAYER_SIZE) ;
-
+        //mPlayerSprite.setSize(PLAYER_SIZE, ( mPlayerSprite.getHeight() / mPlayerSprite.getWidth() ) * PLAYER_SIZE) ;
     }
 
     public void draw(SpriteBatch batch) {
@@ -67,8 +72,10 @@ public class Player {
     public void update() {
         updatePlayerMovement();
 
-        Vector2 pos = mPlayerBody.getPosition();
-        mPlayerSprite.setPosition(pos.x - (mPlayerSprite.getWidth() / 2), pos.y - mPlayerSprite.getHeight() / 4.6f);
+        if(!mSitting) {
+            Vector2 pos = mPlayerBody.getPosition();
+            mPlayerSprite.setPosition(pos.x - (mPlayerSprite.getWidth() / 2), pos.y - mPlayerSprite.getHeight() / 4.6f);
+        }
     }
 
     private void updatePlayerMovement() {
@@ -78,21 +85,24 @@ public class Player {
 
         if(Gdx.input.isKeyPressed(Input.Keys.W)) {
             newSpeed.y = PLAYER_SPEED;
-            mPlayerSprite.setRegion(PLAYER_BACK);
+            mPlayerSprite.setRegion(PLAYER_BACK.getFrame());
         } else if (Gdx.input.isKeyPressed(Input.Keys.S)) {
             newSpeed.y = -PLAYER_SPEED;
-            mPlayerSprite.setRegion(PLAYER_FRONT);
+            mPlayerSprite.setRegion(PLAYER_FRONT.getFrame());
         }
 
         if (Gdx.input.isKeyPressed(Input.Keys.D)) {
             newSpeed.x = PLAYER_SPEED;
-            mPlayerSprite.setRegion(PLAYER_RIGHT);
+            mPlayerSprite.setRegion(PLAYER_RIGHT.getFrame());
         } else if (Gdx.input.isKeyPressed(Input.Keys.A)) {
             newSpeed.x = -PLAYER_SPEED;
-            mPlayerSprite.setRegion(PLAYER_LEFT);
+            mPlayerSprite.setRegion(PLAYER_LEFT.getFrame());
         }
 
         mPlayerBody.setLinearVelocity(newSpeed);
+
+        if(newSpeed.x != 0 || newSpeed.y != 0)
+            mSitting = false;
 
     }
 
@@ -102,5 +112,16 @@ public class Player {
 
     public Vector2 getPosition() {
         return mPlayerBody.getPosition();
+    }
+
+    public void sitDown() {
+        mSitting = true;
+        mPlayerBody.setTransform(75f, 24f, 0f);
+        mPlayerSprite.setPosition(71f, 23f);
+        mPlayerSprite.setRegion(PLAYER_SITTING.getFrame());
+    }
+
+    public boolean isSitting() {
+        return mSitting;
     }
 }
