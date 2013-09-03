ed1246a8afd4e60e4fdfc2c0c2eb7fc74ba26ded
l1nnk <linnkri91@gmail.com>
2013-09-03 10:21:56 +0200

    Fix bookscreen

--- PVUgame/src/no/hist/gruppe5/pvu/book/BookScreen.java
+++ PVUgame/src/no/hist/gruppe5/pvu/book/BookScreen.java
@@ -10,16 +10,10 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import java.io.File;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import no.hist.gruppe5.pvu.Assets;
 import no.hist.gruppe5.pvu.GameScreen;
 import no.hist.gruppe5.pvu.PVU;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import no.hist.gruppe5.pvu.XmlReader;
 
 /**
  *
@@ -31,9 +25,12 @@ public class BookScreen extends GameScreen {
     private Label leftPage;
     private Label rightPage;
     private Stage stage;
+    private int pageNumber;
+    private final String FILE_NAME = "data/test.xml";
 
     public BookScreen(PVU game) {
         super(game);
+        pageNumber = 1;
         initScreen();
     }
 
@@ -43,6 +40,8 @@ public class BookScreen extends GameScreen {
         batch.begin();
         //DRAW BOOK
         batch.end();
+        leftPage.setText(getPageContent(0));
+        rightPage.setText(getPageContent(1));
         stage.draw();
     }
 
@@ -50,6 +49,8 @@ public class BookScreen extends GameScreen {
     protected void update(float delta) {
         if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
             game.setScreen(PVU.MAIN_SCREEN);
+        }if(Gdx.input.isKeyPressed(Input.Keys.SPACE)){
+            getNextPage();
         }
     }
 
@@ -57,26 +58,10 @@ public class BookScreen extends GameScreen {
     protected void cleanUp() {
     }
 
-    public String loadText(String text, DocumentBuilderFactory dbFactory, File bookContent) {
-        try {
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(bookContent);
-            doc.getDocumentElement().normalize();
-            NodeList nodes = doc.getElementsByTagName("book");
-            for (int i = 0; i < nodes.getLength(); i++) {
-                Node node = nodes.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element = (Element) node;
-                    return getValue(text, element);
-                }
-            }
-        } catch (Exception e) {
-        }
-        return "";
-    }
-
-    private String getPageContent() {
-        return null;
+    private String getPageContent(int rightPage) {
+        String name = "section"+(pageNumber+rightPage);
+        String content = XmlReader.loadText(name, FILE_NAME);
+        return content;
     }
 
     private void initScreen() {
@@ -84,30 +69,30 @@ public class BookScreen extends GameScreen {
         testTexture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
 
         stage = new Stage(PVU.GAME_WIDTH, PVU.GAME_HEIGHT, true, batch);
-        File bookContent = new File("data/test.xml");
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 
         Label.LabelStyle labelStyle = new Label.LabelStyle(Assets.primaryFont10px, Color.BLACK);
-        leftPage = new Label(loadText("section1", dbFactory, bookContent).trim(), labelStyle);
+        leftPage = new Label(getPageContent(0), labelStyle);
         leftPage.setFontScale(0.35f);
         leftPage.setWidth(PVU.GAME_WIDTH);
-        leftPage.setPosition(20, PVU.GAME_HEIGHT/2);
+        leftPage.setPosition(20, PVU.GAME_HEIGHT / 2);
         leftPage.setWrap(true);
-        
-        rightPage = new Label(loadText("section1",dbFactory,bookContent).trim(), labelStyle);
+
+        rightPage = new Label(getPageContent(1), labelStyle);
         rightPage.setPosition(110, 0);
         rightPage.setFontScale(0.35f);
         rightPage.setWidth(PVU.GAME_WIDTH);
-        rightPage.setPosition(PVU.GAME_WIDTH-(PVU.GAME_WIDTH*0.35f)-20, PVU.GAME_HEIGHT/2);
+        rightPage.setPosition(PVU.GAME_WIDTH * 0.65f - 20, PVU.GAME_HEIGHT / 2);
         rightPage.setWrap(true);
 
         stage.addActor(leftPage);
         stage.addActor(rightPage);
     }
-
-    private static String getValue(String tag, Element element) {
-        NodeList nodes = element.getElementsByTagName(tag).item(0).getChildNodes();
-        Node node = (Node) nodes.item(0);
-        return node.getNodeValue();
+    
+    public void getNextPage(){
+        pageNumber+=2;
+    }
+    
+    public void getPreviousPage(){
+        pageNumber-=2;
     }
 }
