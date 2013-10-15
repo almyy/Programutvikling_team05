e3618639483cc42d974570097091c4bf656cf580
unknown <froderennemo92@gmail.com>
2013-10-15 11:07:53 +0200

    Refactoring VisionScreen

--- PVUgame/src/no/hist/gruppe5/pvu/visionshooter/VisionScreen.java
+++ PVUgame/src/no/hist/gruppe5/pvu/visionshooter/VisionScreen.java
@@ -6,13 +6,8 @@ import aurelienribon.tweenengine.equations.Quint;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.utils.TimeUtils;
 import java.util.ArrayList;
 import java.util.Random;
@@ -25,55 +20,40 @@ import no.hist.gruppe5.pvu.sound.Sounds;
 
 public class VisionScreen extends GameScreen {
 
-    private int points = 0;
-    private Sprite mVisionDocument;
-    private ShooterShip mVisionShooterShip;
-    private ArrayList<Bullet> shipProjectiles;
+    private ShooterShip mVisionShooterShip = new ShooterShip();
+    
+    //Spawning of elements
+    private ArrayList<Bullet> mShipProjectiles = new ArrayList<Bullet>();
     private long mLastBulletShot = 0;
-    private ArrayList<ShooterElement> elements;
-    private int[] noElements;//Number of elements
-    private ShooterElement[] allElements = new ShooterElement[3];
-    private long lastElementSpawned = 0;
-    private Random random = new Random();
-    private Label pointTextLabel;
-    private Label pointValueLabel;
-    private String pointText = "Points: ";
-    private String pointValue;
-    private Button button;
-    private Skin textboxskin;
-    private Texture tex;
-    private Skin skin = new Skin();
-    private TextField.TextFieldStyle textfieldstyle;
-    private Sounds sound;
+    private ArrayList<ShooterElement> mElements = new ArrayList<ShooterElement>();
+    private int[] mNoElements = {5, 7, 8};//Number of elements: dokument, facebook, youtube
+    private ShooterElement[] mAllElements = {new ShooterFacebook(0), new ShooterYoutube(0), new ShooterDokument(0)};//Used for adding mRandom elements to mElements
+    private long mLastElementSpawned = 0;
+    private Random mRandom = new Random();
+    
+    //Point-handling
+    private int mPoints = 0;
+    private Label mPointTextLabel;
+    private Label mPointValueLabel;
+    private String mPointText = "Points: ";
+    private String mPointValue;
+    private Sounds mSound;
     private TweenManager mTweenManager;
 
     public VisionScreen(PVU game) {
         super(game);
-        mVisionShooterShip = new ShooterShip();
-        shipProjectiles = new ArrayList();
-        elements = new ArrayList();
-        allElements[0] = new ShooterFacebook(0);
-        allElements[1] = new ShooterYoutube(0);
-        allElements[2] = new ShooterDokument(0);
-
-        noElements = new int[3];
-
-        noElements[0] = 5;//Dokument
-        noElements[1] = 7;//Facebook
-        noElements[2] = 8;//Youtube
-
 
         LabelStyle pointStyle = new LabelStyle(Assets.primaryFont10px, Color.BLACK);
-        pointTextLabel = new Label(pointText, pointStyle);
-        pointTextLabel.setFontScale(0.8f);
-        pointTextLabel.setPosition((PVU.GAME_WIDTH * 0.9f) - pointTextLabel.getPrefWidth(), PVU.GAME_HEIGHT * 0.05f);
+        mPointTextLabel = new Label(mPointText, pointStyle);
+        mPointTextLabel.setFontScale(0.8f);
+        mPointTextLabel.setPosition((PVU.GAME_WIDTH * 0.9f) - mPointTextLabel.getPrefWidth(), PVU.GAME_HEIGHT * 0.05f);
 
-        pointValue = "" + points;
-        pointValueLabel = new Label(pointValue, pointStyle);
-        pointValueLabel.setFontScale(0.8f);
-        pointValueLabel.setPosition((PVU.GAME_WIDTH) * 0.87f, PVU.GAME_HEIGHT * 0.05f);
+        mPointValue = "" + mPoints;
+        mPointValueLabel = new Label(mPointValue, pointStyle);
+        mPointValueLabel.setFontScale(0.8f);
+        mPointValueLabel.setPosition((PVU.GAME_WIDTH) * 0.87f, PVU.GAME_HEIGHT * 0.05f);
 
-        sound = new Sounds();
+        mSound = new Sounds();
 
         mTweenManager = new TweenManager();
         Tween.registerAccessor(ShooterElement.class, new ShooterElemementAccessor());
@@ -86,20 +66,12 @@ public class VisionScreen extends GameScreen {
         batch.begin();
         batch.draw(Assets.visionShooterRegion, 0, 0, PVU.GAME_WIDTH, PVU.GAME_HEIGHT);
 
-        if (!shipProjectiles.isEmpty()) {
-            for (int i = 0; i < shipProjectiles.size(); i++) {
-                shipProjectiles.get(i).draw(batch);
-            }
-        }
-        if (!elements.isEmpty()) {
-            for (int i = 0; i < elements.size(); i++) {
-                elements.get(i).draw(batch);
-            }
-        }
+        drawBullets();
+        drawElements();
 
         mVisionShooterShip.draw(batch);
-        pointTextLabel.draw(batch, 1f);
-        pointValueLabel.draw(batch, 1f);
+        mPointTextLabel.draw(batch, 1f);
+        mPointValueLabel.draw(batch, 1f);
 
         batch.end();
     }
@@ -108,125 +80,25 @@ public class VisionScreen extends GameScreen {
     protected void update(float delta) {
         mVisionShooterShip.update(delta);
         if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
-            if ((TimeUtils.millis() - mLastBulletShot) > 800L) {
-                Bullet vB = new Bullet();
-                vB.setProjectileY(mVisionShooterShip.getShipY() + (mVisionShooterShip.getShipHeight() / 2));
-                vB.setProjectileX(mVisionShooterShip.getShipX());
-                shipProjectiles.add(vB);
-                mLastBulletShot = TimeUtils.millis();
-                sound.playSound(0);
-            }
+            shootBullet();
         }
+        updateBulletPos(delta);
+        updateElementPos(delta);
 
-        for (int i = 0; i < shipProjectiles.size(); i++) {
-            if (shipProjectiles.get(i).getProjectileX() < 196) {
-                shipProjectiles.get(i).update(delta);
-            } else {
-                shipProjectiles.remove(i);
-            }
+        checkBulletHit();
+        checkShipHit();
+        
+        if ((TimeUtils.millis() - mLastElementSpawned) > 1500L) {
+            addElement();
         }
-        for (int i = 0; i < elements.size(); i++) {
-            for (int j = 0; j < shipProjectiles.size();) {
-                if (shipProjectiles.get(j).getBulletSprite().getBoundingRectangle().overlaps(elements.get(i).getElementSprite().getBoundingRectangle())) {
-                    if (elements.get(i) instanceof ShooterDokument) {
-                        if ((points - 60) > 0) {
-                            points -= 60;
-                        } else {
-                            points = 0;
-                        }
-                    }
-                    sound.playSound(1);
-                    shipProjectiles.remove(j);
-                    elements.remove(i);
-                    i--;
-                    break;
-                } else {
-                    j++;
-                }
-
-            }
-        }
-        for (int i = 0; i < elements.size(); i++) {
-            if (elements.get(i) instanceof ShooterDokument) {
-                if (elements.get(i).getElementSprite().getBoundingRectangle().overlaps(mVisionShooterShip.getShipSprite().getBoundingRectangle())) {
-                    points += 40;
-                    elements.remove(i);
-                }
-            } else {
-                if (elements.get(i).getElementSprite().getBoundingRectangle().overlaps(mVisionShooterShip.getShipSprite().getBoundingRectangle())) {
-                    if (points - 40 > 0) {
-                        points -= 40;
-                        elements.remove(i);
-
-                    } else {
-                        points = 0;
-                        elements.remove(i);
-
-                    }
-                }
-            }
-        }
-
-
-        if ((TimeUtils.millis() - lastElementSpawned) > 1500L) {
-            int index = random.nextInt(3);
-            ShooterElement i = allElements[index];
-            ShooterElement help = null;
-            if (i instanceof ShooterFacebook && (noElements[1] > 0)) {
-                help = new ShooterFacebook(allElements[index].getElementY());
-                elements.add(help);
-                noElements[1]--;
-            } else if (i instanceof ShooterYoutube && (noElements[2] > 0)) {
-                help = new ShooterYoutube(allElements[index].getElementY());
-                elements.add(help);
-                noElements[2]--;
-            } else if (noElements[0] > 0) {
-                help = new ShooterDokument(allElements[index].getElementY());
-                elements.add(help);
-                noElements[0]--;
-            }
-
-            if (help != null) {
-                help.setElementY(random.nextInt(Math.round(PVU.GAME_HEIGHT - help.getElementHeight())));
-                help.setElementX(200f);
-                Tween.to(help, ShooterElemementAccessor.POS_X, 1f)
-                        .target(160f)
-                        .ease(Quint.IN)
-                        .start(mTweenManager);
-            }
-
-            lastElementSpawned = TimeUtils.millis();
-        }
-
-        for (int i = 0; i < elements.size(); i++) {
-            if (elements.get(i).getElementX() > 0) {
-                elements.get(i).update(delta);
-            } else if ((points - 20) > 0) {
-                points -= 20;
-                elements.remove(i);
-            } else {
-                points = 0;
-                elements.remove(i);
-            }
-        }
-        pointValue = "" + points;
-        pointValueLabel.setText(pointValue);
-
-        if (elements.isEmpty() && finish()) {
-            ScoreHandler.updateScore(0, points);
-            pointValueLabel.setFontScale(2f);
-            pointTextLabel.setFontScale(2f);
-            pointValueLabel.setPosition(pointTextLabel.getX() + pointTextLabel.getPrefWidth(), PVU.GAME_HEIGHT / 2);
-            pointTextLabel.setPosition((PVU.GAME_WIDTH / 2) - pointTextLabel.getPrefWidth() / 2, PVU.GAME_HEIGHT / 2);
-
-        }
-
+        mPointValue = "" + mPoints;
+        mPointValueLabel.setText(mPointValue);
+        setScore();
+        
         if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
             game.setScreen(PVU.MAIN_SCREEN);
         }
-
         mTweenManager.update(delta);
-
     }
 
     @Override
@@ -235,10 +107,150 @@ public class VisionScreen extends GameScreen {
 
     private boolean finish() {
         for (int i = 0; i < 3; i++) {
-            if (noElements[i] > 0) {
+            if (mNoElements[i] > 0) {
                 return false;
             }
         }
         return true;
+    }
+
+    private void setScore() {
+        if (mElements.isEmpty() && finish()) {
+            ScoreHandler.updateScore(0, mPoints);
+            mPointValueLabel.setFontScale(2f);
+            mPointTextLabel.setFontScale(2f);
+            mPointValueLabel.setPosition(mPointTextLabel.getX() + mPointTextLabel.getPrefWidth(), PVU.GAME_HEIGHT / 2);
+            mPointTextLabel.setPosition((PVU.GAME_WIDTH / 2) - mPointTextLabel.getPrefWidth() / 2, PVU.GAME_HEIGHT / 2);
+
+        }
+    }
+
+    private void shootBullet() {
+        if ((TimeUtils.millis() - mLastBulletShot) > 800L) {
+            Bullet vB = new Bullet();
+            vB.setProjectileY(mVisionShooterShip.getShipY() + (mVisionShooterShip.getShipHeight() / 2));
+            vB.setProjectileX(mVisionShooterShip.getShipX());
+            mShipProjectiles.add(vB);
+            mLastBulletShot = TimeUtils.millis();
+            mSound.playSound(0);
+        }
+
+    }
+
+    private void updateBulletPos(float delta) {
+        for (int i = 0; i < mShipProjectiles.size(); i++) {
+            if (mShipProjectiles.get(i).getProjectileX() < 196) {
+                mShipProjectiles.get(i).update(delta);
+            } else {
+                mShipProjectiles.remove(i);
+            }
+        }
+    }
+
+    private void updateElementPos(float delta) {
+        for (int i = 0; i < mElements.size(); i++) {
+            if (mElements.get(i).getElementX() > 0) {
+                mElements.get(i).update(delta);
+            } else if ((mPoints - 20) > 0) {
+                mPoints -= 20;
+                mElements.remove(i);
+            } else {
+                mPoints = 0;
+                mElements.remove(i);
+            }
+        }
+    }
+
+    private void drawBullets() {
+        if (!mShipProjectiles.isEmpty()) {
+            for (int i = 0; i < mShipProjectiles.size(); i++) {
+                mShipProjectiles.get(i).draw(batch);
+            }
+        }
+    }
+
+    private void drawElements() {
+        if (!mElements.isEmpty()) {
+            for (int i = 0; i < mElements.size(); i++) {
+                mElements.get(i).draw(batch);
+            }
+        }
+    }
+
+    private void addElement() {//adds element to the arrray-list
+        int index = mRandom.nextInt(3);
+        ShooterElement i = mAllElements[index];
+        ShooterElement help = null;
+        if (i instanceof ShooterFacebook && (mNoElements[1] > 0)) {
+            help = new ShooterFacebook(mAllElements[index].getElementY());
+            mElements.add(help);
+            mNoElements[1]--;
+        } else if (i instanceof ShooterYoutube && (mNoElements[2] > 0)) {
+            help = new ShooterYoutube(mAllElements[index].getElementY());
+            mElements.add(help);
+            mNoElements[2]--;
+        } else if (mNoElements[0] > 0) {
+            help = new ShooterDokument(mAllElements[index].getElementY());
+            mElements.add(help);
+            mNoElements[0]--;
+        }
+
+        if (help != null) {
+            help.setElementY(mRandom.nextInt(Math.round(PVU.GAME_HEIGHT - help.getElementHeight())));
+            help.setElementX(200f);
+            Tween.to(help, ShooterElemementAccessor.POS_X, 1f)
+                    .target(160f)
+                    .ease(Quint.IN)
+                    .start(mTweenManager);
+        }
+
+        mLastElementSpawned = TimeUtils.millis();
+    }
+
+    private void checkBulletHit() {
+        for (int i = 0; i < mElements.size(); i++) {
+            for (int j = 0; j < mShipProjectiles.size();) {
+                if (mShipProjectiles.get(j).getBulletSprite().getBoundingRectangle().overlaps(mElements.get(i).getElementSprite().getBoundingRectangle())) {
+                    if (mElements.get(i) instanceof ShooterDokument) {
+                        if ((mPoints - 60) > 0) {
+                            mPoints -= 60;
+                        } else {
+                            mPoints = 0;
+                        }
+                    }
+                    mSound.playSound(1);
+                    mShipProjectiles.remove(j);
+                    mElements.remove(i);
+                    i--;
+                    break;
+                } else {
+                    j++;
+                }
+
+            }
+        }
+    }
+
+    private void checkShipHit() {
+        for (int i = 0; i < mElements.size(); i++) {
+            if (mElements.get(i) instanceof ShooterDokument) {
+                if (mElements.get(i).getElementSprite().getBoundingRectangle().overlaps(mVisionShooterShip.getShipSprite().getBoundingRectangle())) {
+                    mPoints += 40;
+                    mElements.remove(i);
+                }
+            } else {
+                if (mElements.get(i).getElementSprite().getBoundingRectangle().overlaps(mVisionShooterShip.getShipSprite().getBoundingRectangle())) {
+                    if (mPoints - 40 > 0) {
+                        mPoints -= 40;
+                        mElements.remove(i);
+
+                    } else {
+                        mPoints = 0;
+                        mElements.remove(i);
+
+                    }
+                }
+            }
+        }
     }
 }
