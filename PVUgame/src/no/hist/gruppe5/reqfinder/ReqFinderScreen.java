3e8390cc02352014f30be966cf67d177412099c0
almyy <martinalmvik@hotmail.com>
2013-10-21 13:20:17 +0200

    reqfinder add

---
+++ PVUgame/src/no/hist/gruppe5/reqfinder/ReqFinderScreen.java
@@ -0,0 +1,28 @@
+package no.hist.gruppe5.reqfinder;
+
+import no.hist.gruppe5.pvu.GameScreen;
+import no.hist.gruppe5.pvu.PVU;
+
+/**
+ *
+ * @author Martin
+ */
+public class ReqFinderScreen extends GameScreen {
+
+    
+    public ReqFinderScreen(PVU pvu) {
+        super(pvu);
+    }
+    @Override
+    protected void draw(float delta) {
+    }
+
+    @Override
+    protected void update(float delta) {
+    }
+
+    @Override
+    protected void cleanUp() {
+    }
+    
+}
